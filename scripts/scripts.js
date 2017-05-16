8d99405808e7e52fd7a52c0fb1833f0b258667a1
Jason Boser <jasonboser@gmail.com>
2017-05-16 13:15:13 -0500

    cherries and keep moving

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,9 +1,10 @@
+//0 = empty, 1 = coin, 2 = wall, 3 = cherry
 var world = [
             [2,2,2,2,2,2,2,2,2,2],
             [2,0,1,2,1,1,1,1,1,2],
             [2,1,1,2,1,2,2,2,1,2],
             [2,1,1,2,1,2,1,2,1,2],
-            [0,1,1,2,1,2,1,2,1,0],
+            [0,1,1,2,3,2,1,2,1,0],
             [2,1,1,2,2,2,1,2,1,2],
             [2,1,1,1,1,1,1,2,1,2],
             [2,1,1,1,1,1,1,1,1,2],
@@ -17,7 +18,7 @@ var pacman = {
     y: 1
 };
 
-var direction
+var movement;
 
 function displayWorld(){
     var output = '';
@@ -27,7 +28,9 @@ function displayWorld(){
     for (var i=0; i < world.length; i++){
         output += "<div class='row'>";
         for (var j=0; j < world[i].length; j++){
-            if (world[i][j] == 2)
+            if (world[i][j] == 3)
+                output += "<div class='cherry'></div>";
+            else if (world[i][j] == 2)
                 output += "<div class='brick'></div>";
             else if (world[i][j] == 1)
                 output += "<div class='coin'></div>";
@@ -48,28 +51,40 @@ function displayScore(){
     document.getElementById('score').innerHTML = score;
 }
 
-document.onkeydown = function(e){
-    if (e.keyCode == 37 && world[pacman.y][pacman.x-1] != 2){
-        pacman.x--;
+function movePacman(e){
+    if (e.keyCode == 37){
+        //left
         $('#pacman').css("transform","rotate(180deg)");
-        if(pacman.x==-1){
-            pacman.x = world[pacman.y].length-1;
+        if(world[pacman.y][pacman.x-1] != 2){
+            pacman.x--;
+            if(pacman.x==-1){
+                pacman.x = world[pacman.y].length-1;
+            }
         }
     }
-    else if(e.keyCode == 39 && world[pacman.y][pacman.x+1] != 2){
-        pacman.x++;
+    else if(e.keyCode == 39){
+        //right
         $('#pacman').css("transform","rotate(0deg)");
-        if(pacman.x==world[pacman.y].length){
-            pacman.x = 0;
+        if(world[pacman.y][pacman.x+1] != 2){
+            pacman.x++;
+            if(pacman.x==world[pacman.y].length){
+                pacman.x = 0;
+            }
         }
     }
-    else if(e.keyCode == 38 && world[pacman.y-1][pacman.x] != 2){
-        pacman.y--;
+    else if(e.keyCode == 38){
+        //up
         $('#pacman').css("transform","rotate(270deg)");
+        if(world[pacman.y-1][pacman.x] != 2){
+            pacman.y--;
+        }
     }
-    else if(e.keyCode == 40 && world[pacman.y+1][pacman.x] != 2){
-        pacman.y++;
+    else if(e.keyCode == 40){
+        //down
         $('#pacman').css("transform","rotate(90deg)");
+        if(world[pacman.y+1][pacman.x] != 2){
+            pacman.y++;
+        }
     }
     
     if (world[pacman.y][pacman.x] == 1){
@@ -77,9 +92,22 @@ document.onkeydown = function(e){
         score+=10;
         displayWorld();
         displayScore();
+    }else if(world[pacman.y][pacman.x] == 3){
+        world[pacman.y][pacman.x] = 0;
+        score+=100;
+        displayWorld();
+        displayScore();
     }
 
     displayPacman();
+}
+
+document.onkeydown = function(e){
+    clearInterval(movement);
+    movePacman(e);
+    movement = setInterval(function(){
+        movePacman(e);
+    },400);
 };
 
 $(document).ready(function(){
